dbd5415aab2074f3d8299843512f9e6ba34980e0
Sayyed-Abrar-Akhtar <akhtars10@uni.coventry.ac.uk>
2021-01-22 18:28:33 +0545

    Added content to Category recyclerView

--- app/src/main/java/com/sayyed/onlineclothingapplication/DashboardActivity.kt
+++ app/src/main/java/com/sayyed/onlineclothingapplication/DashboardActivity.kt
@@ -2,10 +2,29 @@ package com.sayyed.onlineclothingapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.sayyed.onlineclothingapplication.adapter.CategoryAdapter
+import com.sayyed.onlineclothingapplication.models.Categories
 
 class DashboardActivity : AppCompatActivity() {
+
+    private var categoriesList: ArrayList<Categories>()
+    private lateinit var recyclerViewCategory: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        recyclerViewCategory = findViewById(R.id.recyclerViewCategory)
+
+        initialCategoryData()
+
+        val adapter = CategoryAdapter(categoriesList, this@DashboardActivity)
+        recyclerViewCategory.adapter = adapter
+
+    }
+
+    private fun initialCategoryData() {
+        TODO("Not yet implemented")
     }
 }
