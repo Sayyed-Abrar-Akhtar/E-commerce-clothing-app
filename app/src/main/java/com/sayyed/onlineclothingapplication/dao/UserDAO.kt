04953a702a218a84a64eaf5f7b06002c8c465423
Sayyed-Abrar-Akhtar <akhtars10@uni.coventry.ac.uk>
2021-01-28 23:18:29 +0545

    Insert query and select query created in UserDAO

--- app/src/main/java/com/sayyed/onlineclothingapplication/dao/UserDAO.kt
+++ app/src/main/java/com/sayyed/onlineclothingapplication/dao/UserDAO.kt
@@ -2,6 +2,7 @@ package com.sayyed.onlineclothingapplication.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.sayyed.onlineclothingapplication.entities.User
 
 @Dao
@@ -12,6 +13,9 @@ interface UserDAO {
     suspend fun insertUser(user: User) {
 
     }
+
+    @Query("select * from User where username = (:username) and password= (:password)")
+    suspend fun isUserValid(username: String, password: String): User
 
 
 }
