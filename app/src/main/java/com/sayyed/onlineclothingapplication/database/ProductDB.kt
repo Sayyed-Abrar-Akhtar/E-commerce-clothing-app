6280561179195ff32905a44f59a640a522ac2714
Sayyed-Abrar-Akhtar <akhtars10@uni.coventry.ac.uk>
2021-02-28 16:54:42 +0545

    added getInstance function

--- app/src/main/java/com/sayyed/onlineclothingapplication/database/ProductDB.kt
+++ app/src/main/java/com/sayyed/onlineclothingapplication/database/ProductDB.kt
@@ -1,7 +1,10 @@
 package com.sayyed.onlineclothingapplication.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.sayyed.onlineclothingapplication.dao.ProductDAO
 import com.sayyed.onlineclothingapplication.entities.Product
 
 
@@ -11,4 +14,27 @@ import com.sayyed.onlineclothingapplication.entities.Product
         exportSchema = false
 )
 abstract class ProductDB : RoomDatabase() {
+
+    abstract val productDAO: ProductDAO
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: ProductDB? = null
+
+        fun getInstance(context: Context): ProductDB {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            ProductDB::class.java,
+                            "product_database"
+                    ).build()
+                }
+                return instance
+            }
+        }
+    }
 }
+
