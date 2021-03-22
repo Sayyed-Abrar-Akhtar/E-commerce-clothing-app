f723fcf8e347a0d3a4ee8b29f89bfb8d83191d1a
Sayyed-Abrar-Akhtar <akhtars10@uni.coventry.ac.uk>
2021-03-22 11:35:41 +0545

    Added dependency in gradle

--- app/src/main/java/com/sayyed/onlineclothingapplication/response/CategoryResponse.kt
+++ app/src/main/java/com/sayyed/onlineclothingapplication/response/CategoryResponse.kt
@@ -2,4 +2,4 @@ package com.sayyed.onlineclothingapplication.response
 
 import com.sayyed.onlineclothingapplication.models.Category
 
-data class CategoryResponse ( val category: ArrayList<Category> )
+data class CategoryResponse ( val category: ArrayList<Category>, val success: Boolean )
